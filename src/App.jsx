96ebb392e265bff3a4aef9b2f8f2f8a2ea2ea0bd
mihkuno <caindayjoeninyo@gmail.com>
2024-01-19 07:27:05 +0800

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
 import { Image, Text, VStack, HStack, Button, Center } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
 function App() {
 	return (
 		<>
+			{/* background polygon */}
 			<Image src="src/assets/polygon.svg" alt="Polygon" pos="absolute" zIndex={-1} right={0} />
+
+			{/* navigation bar */}
+			<NavBar />
+
 			<Center minH="100vh">
 				<HStack>
 					<VStack align="flex-start" maxW={'lg'} spacing={5}>
+						{/* headline */}
 						<Text fontWeight={'bold'} fontSize={70} lineHeight={1} color={'gray.800'}>
 							Lory's Dried Fish
 						</Text>
+
+						{/* description */}
 						<Text>
 							Offering a diverse selection of repacked dried fish sourced from the rich
 							fisheries of the Visayas and Mindanao, setting the standard for{' '}
 							<b>hygienic and high-quality seafood products.</b>
 						</Text>
+
+						{/* login button */}
 						<Button colorScheme="blue" fontSize={14} mt={5}>
-							Log in with Google
+							Sign in with Google
 						</Button>
 					</VStack>
+					{/* logo image */}
 					<Image src="src/assets/logo.png" alt="Logo" maxW={450} />
 				</HStack>
 			</Center>
