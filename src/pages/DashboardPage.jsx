e62532a763bf58d50be684a470a070423b67a69d
mihkuno <caindayjoeninyo@gmail.com>
2024-01-30 09:17:49 +0800

    adjusted wallet card font size

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -7,23 +7,23 @@ function WalletCard({ Icon, title, subtitle, amount }) {
 	return (
 		<VStack
 			justifyContent={'center'}
-			w={220}
+			w={200}
 			h={240}
 			spacing={1}
 			backgroundColor={'#fff'}
 			borderRadius={15}
 			boxShadow="0px 2px 5.5px rgba(0, 0, 0, 0.02)">
 			<Box background={'#4FD1C5'} padding={4} borderRadius={12} mb={3}>
-				<Icon color={'white'} width={'22px'} height={'22px'} />
+				<Icon color={'white'} width={'20px'} height={'20px'} />
 			</Box>
-			<Text fontWeight={'bold'} color={'#2D3748'}>
+			<Text fontWeight={'bold'} fontSize={15} color={'#2D3748'}>
 				{title}
 			</Text>
 			<Text fontWeight={'bold'} fontSize={12} color={'#A0AEC0'}>
 				{subtitle}
 			</Text>
 			<Image src="../src/assets/dashboard_card_divider.svg" zIndex={1} mt={3} mb={3} />
-			<Text fontWeight={'bold'} fontSize={16} color={'#2D3748'}>
+			<Text fontWeight={'bold'} fontSize={15} color={'#2D3748'}>
 				₱{amount}
 			</Text>
 		</VStack>
